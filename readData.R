fffbf8eebcd3af4e0e3e1272c7cf9885ae4a9a7c
csx <781244184@qq.com>
2022-01-05 22:26:09 +0800

    add read data

---
+++ readData.R
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+setwd("/Users/csx/GitProject/HEToGeneSignal")
+
+library("EBImage")
+images1 <- "/media/qsong/piResult/01.liver_spatial/02.liver_spatial_ST/02.image/V10N16-279-A-4-2-3.tif"
+
+f <- system.file("images", images1, package="EBImage")
+imgcol2 = readImage(f)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
