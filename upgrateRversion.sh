4582040982f8028029978358277cbd34ac7a86d1
csx <781244184@qq.com>
2022-01-06 10:00:55 +0800

    upgrate version

---
+++ upgrateRversion.sh
@@ -0,0 +1,8 @@
+
+
+
+echo "deb http://www.stats.bris.ac.uk/R/bin/linux/ubuntu precise/" >> /etc/apt/sources.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
+apt-get update
+apt-get upgrade
+
